2a441d7a3f0050f784652e6a0895687b7fd60463
ZdenekVitek <zdenek.vitek@atsco.cz>
2017-10-30 20:57:13 +0100

    (PHP 4 >= 4.1.0, PHP 5, PHP 7) $_GET -- $HTTP_GET_VARS [deprecated] — HTTP GET variables
    (PHP 4, PHP 5)
    mysql_pconnect — Open a persistent connection to a MySQL server
    Warning
    This extension was deprecated in PHP 5.5.0, and it was removed in PHP 7.0.0.
    Instead, the MySQLi or PDO_MySQL extension should be used. See also MySQL: choosing an API guide and related FAQ for more information. Alternatives to this function include:
    mysqli_connect() with p: host prefix
    
    (PHP 4, PHP 5)
    mysql_select_db — Select a MySQL database
    Warning
    This extension was deprecated in PHP 5.5.0, and it was removed in PHP 7.0.0.
    Instead, the MySQLi or PDO_MySQL extension should be used.
    
    (PHP 4, PHP 5)
    mysql_query — Send a MySQL query
    
    Warning
    This extension was deprecated in PHP 5.5.0, and it was removed in PHP 7.0.0.
    Instead, the MySQLi or PDO_MySQL extension should be used.
    (PHP 4, PHP 5)
    mysql_num_rows — Get number of rows in result
    Warning
    This extension was deprecated in PHP 5.5.0, and it was removed in PHP 7.0.0.
    Instead, the MySQLi or PDO_MySQL extension should be used.
    
    (PHP 4, PHP 5)
    mysql_fetch_array — Fetch a result row as an associative array, a numeric array, or both
    
    Warning
    This extension was deprecated in PHP 5.5.0, and it was removed in PHP 7.0.0.
    Instead, the MySQLi or PDO_MySQL extension should be used.

--- novinky/aktivace.php
+++ novinky/aktivace.php
@@ -8,43 +8,40 @@
     <h1>Novinky e-mailem</h1>
     <hr />
 <?php
-  
-  $mail_nov=$HTTP_GET_VARS[mail_nov];
+  $mail_nov=$_GET[mail_nov];
   $dbhost="localhost";
   $dbusr="czxhtml-php";
   $dbpassw="passw_xhtml-php";
   $dbname="czxhtml-php";
   //připojení k databázovému serveru
-  if(@$link=mysql_pconnect($dbhost,$dbusr,$dbpassw)) 
+  if(@$link=mysqli_connect($dbhost,$dbusr,$dbpassw)) 
     echo "<p class='nadpis'>Spojení se serverem bylo úspěšně navázáno.</p>"; 
   else die("<p class='nadpis'>Server nepřipojen !!!</p>");
   //výběr databáze
-  if(@$db=mysql_select_db($dbname,$link)) 
+  if(@$db=mysqli_select_db($link,$dbname)) 
     echo "<p class='nadpis'>Databáze byla otevřena</p>"; 
   else die("<p class='nadpis'>Databáze nebyla nalezena !!!</p>");
   //zjištění počtu záznamů
   $dotaz = "SELECT * FROM novinky";
-  $result=mysql_query($dotaz,$link);
-  $pocet = mysql_num_rows($result);
+  $result=mysqli_query($link,$dotaz);
+  $pocet = mysqli_num_rows($result);
   echo "<p class='nadpis'>Databáze obsahuje celkem: $pocet záznamů.</p>";
-  while($polozka=mysql_fetch_array($result)) echo "$polozka[mail]<br />";
+  while($polozka=mysqli_fetch_array($result)) echo "$polozka[mail]<br />";
   //zjištění existence zaslané adresy
   $dotaz = "SELECT * FROM novinky WHERE mail='$mail_nov'";
-  $result=mysql_query($dotaz,$link);
-  $pocet = mysql_num_rows($result);
+  $result=mysqli_query($link,$dotaz);
+  $pocet = mysqli_num_rows($result);
   if($pocet==1) {
     echo "<p class='nadpis'>Adresa $mail_nov je v databázi";
     $dotaz = "DELETE FROM novinky WHERE mail='$mail_nov'";
-    $result=mysql_query($dotaz,$link);
+    $result=mysqli_query($link,$dotaz);
     echo "<p class='nadpis'>e-mailová adresa $mail_nov byla deaktivována</p>";
   }
   else {
     echo "<p class='nadpis'>Adresa $mail_nov není v databázi";
     $dotaz="INSERT INTO novinky VALUE('$mail_nov')";
-    $result=mysql_query($dotaz,$link);
+    $result=mysqli_query($link,$dotaz);
     echo "<p class='nadpis'>e-mailová adresa $mail_nov byla aktivována</p>";
   }
-
-
   konec();
 ?>
